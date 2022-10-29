a6186713f6ad3cb16fa99bb3b90480546d2394a2
analauquispe <anilaoq@gmail.com>
2022-10-29 01:43:08 -0300

    header

--- FRONTEND_AnaQuispe/src/app/app.module.ts
+++ FRONTEND_AnaQuispe/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SobremiComponent } from './components/sobremi/sobremi.component';
     EducacionComponent,
     HomeComponent,
     LoginComponent,
-    SobremiComponent
+    SobremiComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
